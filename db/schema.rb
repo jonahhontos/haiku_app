b9d9bec06bef8127a040ca4aaaef58964f0a3561
Jonah Hontos <jonahhontos@gmail.com>
2016-02-17 12:19:38 -0800

    added relation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217201512) do
+ActiveRecord::Schema.define(version: 20160217201908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 20160217201512) do
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "haikus", ["user_id"], name: "index_haikus_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -29,4 +32,5 @@ ActiveRecord::Schema.define(version: 20160217201512) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "haikus", "users"
 end
